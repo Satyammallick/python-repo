41f1c307af0146c6bd8343c71e56b4d69651cfca
Satyam Malik <147387663+Satyammallick@users.noreply.github.com>
2026-06-13 19:51:44 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,14 @@
+friendsName = ["Ram","Hari","Jitu","Cat","Ram"]
+print(friendsName)
+
+print(type(friendsName))
+
+"""friendsNameList = ["Ram","Hari"]
+print(type(friendsNameList))
+print(friendsNameList)
+friendsNameList.append("Sita")
+friendsNameList.append("Gita")
+print(friendsNameList)
+del friendsNameList[1]
+#friendsNameList.pop()
+print(friendsNameList)"""
